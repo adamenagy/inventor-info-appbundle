4b1d81975a00db45e0641cc2ce9e9266766764b4
Adam Nagy <adam_e_nagy@hotmail.com>
2019-10-21 10:28:45 +0100

    Using the same code from both Run and RunWithArguments

--- DA4I_BasicInfo/DA4I_BasicInfoPlugin/SampleAutomation.cs
+++ DA4I_BasicInfo/DA4I_BasicInfoPlugin/SampleAutomation.cs
@@ -25,146 +25,147 @@ using System.Threading;
 
 namespace DA4I_BasicInfoPlugin
 {
-    [ComVisible(true)]
-    public class SampleAutomation
+  [ComVisible(true)]
+  public class SampleAutomation
+  {
+    InventorServer inventorApplication;
+
+    public SampleAutomation(InventorServer inventorApp)
     {
-        InventorServer inventorApplication;
-
-        public SampleAutomation(InventorServer inventorApp)
-        {
-            inventorApplication = inventorApp;
-        }
-
-        public void Run(Document doc)
-        {
-            LogTrace("Run called with {0}", doc.DisplayName);
-        }
-
-        private class HeartBeat : IDisposable
-        {
-            // default is 50s
-            public HeartBeat(int intervalMillisec = 50000)
-            {
-                t = new Thread(() =>
-                {
-
-                    LogTrace("HeartBeating every {0}ms.", intervalMillisec);
-
-                    for (; ; )
-                    {
-                        Thread.Sleep((int)intervalMillisec);
-                        LogTrace("HeartBeat {0}.", (long)(new TimeSpan(DateTime.Now.Ticks - ticks).TotalSeconds));
-                    }
-
-                });
-
-                ticks = DateTime.Now.Ticks;
-                t.Start();
-            }
-
-            public void Dispose()
-            {
-                Dispose(true);
-                GC.SuppressFinalize(this);
-            }
-
-            protected virtual void Dispose(bool disposing)
-            {
-                if (disposing)
-                {
-                    if (t != null)
-                    {
-                        LogTrace("Ending HeartBeat");
-                        t.Abort();
-                        t = null;
-                    }
-                }
-            }
-
-            private Thread t;
-            private long ticks;
-        }
-
-
-
-        public void RunWithArguments(Document doc, NameValueMap map)
-        {
-            LogTrace("Processing " + doc.FullFileName);
-
-            try
-            {
-                if (doc.DocumentType == DocumentTypeEnum.kPartDocumentObject || 
-                    doc.DocumentType == DocumentTypeEnum.kAssemblyDocumentObject)
-                {
-                    using (new HeartBeat())
-                    {
-                        dynamic ddoc = doc;
-
-                        // Extents
-                        Point mp = ddoc.ComponentDefinition.RangeBox.MinPoint;
-                        Point xp = ddoc.ComponentDefinition.RangeBox.MaxPoint;
-
-                        Double x = xp.X - mp.X;
-                        Double y = xp.Y - mp.Y;
-                        Double z = xp.Z - mp.Z;
-
-                        // Surface and Volume
-                        Double s = ddoc.ComponentDefinition.MassProperties.Area;
-                        Double v = ddoc.ComponentDefinition.MassProperties.Volume;
-
-                        // Svae it to result.json file
-                        string result = string.Format(
-                            "{{ \"extent\": {{" +
-                                    "\"x\": {0}," +
-                                    "\"y\": {1}," +
-                                    "\"z\": {2} }}," +
-                                "\"surface\": {3}," +
-                                "\"volume\": {4} }}",
-                            x, y, z, s, v);
-                        System.IO.File.WriteAllText("result.json", result);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                LogError("Processing failed. " + e.ToString());
-            }
-        }
-
-        #region Logging utilities
-
-        /// <summary>
-        /// Log message with 'trace' log level.
-        /// </summary>
-        private static void LogTrace(string format, params object[] args)
-        {
-            Trace.TraceInformation(format, args);
-        }
-
-        /// <summary>
-        /// Log message with 'trace' log level.
-        /// </summary>
-        private static void LogTrace(string message)
-        {
-            Trace.TraceInformation(message);
-        }
-
-        /// <summary>
-        /// Log message with 'error' log level.
-        /// </summary>
-        private static void LogError(string format, params object[] args)
-        {
-            Trace.TraceError(format, args);
-        }
-
-        /// <summary>
-        /// Log message with 'error' log level.
-        /// </summary>
-        private static void LogError(string message)
-        {
-            Trace.TraceError(message);
-        }
-
-        #endregion
+      inventorApplication = inventorApp;
     }
+
+    public void Run(Document doc)
+    {
+      LogTrace("Run called with {0}", doc.DisplayName);
+
+      try
+      {
+        if (doc.DocumentType == DocumentTypeEnum.kPartDocumentObject ||
+            doc.DocumentType == DocumentTypeEnum.kAssemblyDocumentObject)
+        {
+          using (new HeartBeat())
+          {
+            dynamic ddoc = doc;
+
+            // Extents
+            Point mp = ddoc.ComponentDefinition.RangeBox.MinPoint;
+            Point xp = ddoc.ComponentDefinition.RangeBox.MaxPoint;
+
+            Double x = xp.X - mp.X;
+            Double y = xp.Y - mp.Y;
+            Double z = xp.Z - mp.Z;
+
+            // Surface and Volume
+            Double s = ddoc.ComponentDefinition.MassProperties.Area;
+            Double v = ddoc.ComponentDefinition.MassProperties.Volume;
+
+            // Svae it to result.json file
+            string result = string.Format(
+                "{{ \"extent\": {{" +
+                        "\"x\": {0}," +
+                        "\"y\": {1}," +
+                        "\"z\": {2} }}," +
+                    "\"surface\": {3}," +
+                    "\"volume\": {4} }}",
+                x, y, z, s, v);
+            System.IO.File.WriteAllText("result.json", result);
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        LogError("Processing failed. " + e.ToString());
+      }
+    }
+
+    private class HeartBeat : IDisposable
+    {
+      // default is 50s
+      public HeartBeat(int intervalMillisec = 50000)
+      {
+        t = new Thread(() =>
+        {
+
+          LogTrace("HeartBeating every {0}ms.", intervalMillisec);
+
+          for (; ; )
+          {
+            Thread.Sleep((int)intervalMillisec);
+            LogTrace("HeartBeat {0}.", (long)(new TimeSpan(DateTime.Now.Ticks - ticks).TotalSeconds));
+          }
+
+        });
+
+        ticks = DateTime.Now.Ticks;
+        t.Start();
+      }
+
+      public void Dispose()
+      {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+      }
+
+      protected virtual void Dispose(bool disposing)
+      {
+        if (disposing)
+        {
+          if (t != null)
+          {
+            LogTrace("Ending HeartBeat");
+            t.Abort();
+            t = null;
+          }
+        }
+      }
+
+      private Thread t;
+      private long ticks;
+    }
+
+
+
+    public void RunWithArguments(Document doc, NameValueMap map)
+    {
+      LogTrace("RunWithArguments called with {0}", doc.DisplayName);
+      Run(doc);
+    }
+
+    #region Logging utilities
+
+    /// <summary>
+    /// Log message with 'trace' log level.
+    /// </summary>
+    private static void LogTrace(string format, params object[] args)
+    {
+      Trace.TraceInformation(format, args);
+    }
+
+    /// <summary>
+    /// Log message with 'trace' log level.
+    /// </summary>
+    private static void LogTrace(string message)
+    {
+      Trace.TraceInformation(message);
+    }
+
+    /// <summary>
+    /// Log message with 'error' log level.
+    /// </summary>
+    private static void LogError(string format, params object[] args)
+    {
+      Trace.TraceError(format, args);
+    }
+
+    /// <summary>
+    /// Log message with 'error' log level.
+    /// </summary>
+    private static void LogError(string message)
+    {
+      Trace.TraceError(message);
+    }
+
+    #endregion
+  }
 }
